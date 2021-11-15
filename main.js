e4ea131cbebe6db241cb970969ba1d06cf6f1eed
Julien Serme <julienserme@yahoo.com>
2021-11-15 17:01:07 +0100

    Tableau JS

--- main.js
+++ main.js
@@ -7,4 +7,46 @@ const tabAlbum = [
     genre: "Rap",
     duree: "73",
   },
+  {
+    nom: "Lust for Life",
+    artiste: "Iggy Pop",
+    label: "RCA Records",
+    annee: "1977",
+    genre: "Rock",
+    duree: "41",
+  },
+  {
+    nom: "Si Dieu Veut",
+    artiste: "Fonky Family",
+    label: "Les disques Mont Réal",
+    annee: "1998",
+    genre: "Rap",
+    duree: "67",
+  },
+  {
+    nom: "A Night At The Opera",
+    artiste: "Queen",
+    label: "EMI",
+    annee: "1975",
+    genre: "Rock",
+    duree: "43",
+  },
+  {
+    nom: "Entre gris clair et gris foncé",
+    artiste: "Jean Jacques Goldmann",
+    label: "Epic",
+    annee: "1987",
+    genre: "Pop",
+    duree: "83",
+  },
+  {
+    nom: "Discovery",
+    artiste: "Daft Punk",
+    label: "Parlophone",
+    annee: "2001",
+    genre: "House",
+    duree: "61",
+  },
 ];
+
+document.addEventListener("DOMContentLoaded", function () {});
