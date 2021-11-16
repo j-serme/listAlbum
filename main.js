a705051b75bd23bd9b9176981d5e26e8a5eef00e
Julien Serme <julienserme@yahoo.com>
2021-11-16 14:43:30 +0100

    Ajout des images, Fonction display

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const tabAlbum = [
+const albums = [
   {
+    image: "Micro_Argent",
     nom: "l'école du micro d'argent",
     artiste: "Iam",
     label: "EMI",
@@ -8,6 +9,7 @@ const tabAlbum = [
     duree: "73",
   },
   {
+    image: "lust_For_Life",
     nom: "Lust for Life",
     artiste: "Iggy Pop",
     label: "RCA Records",
@@ -16,6 +18,7 @@ const tabAlbum = [
     duree: "41",
   },
   {
+    image: "si_Dieu_Veut",
     nom: "Si Dieu Veut",
     artiste: "Fonky Family",
     label: "Les disques Mont Réal",
@@ -24,6 +27,7 @@ const tabAlbum = [
     duree: "67",
   },
   {
+    image: "A Night At The Opera",
     nom: "A Night At The Opera",
     artiste: "Queen",
     label: "EMI",
@@ -32,6 +36,7 @@ const tabAlbum = [
     duree: "43",
   },
   {
+    image: "Entre Gris Clair et Gris Foncé",
     nom: "Entre gris clair et gris foncé",
     artiste: "Jean Jacques Goldmann",
     label: "Epic",
@@ -40,6 +45,7 @@ const tabAlbum = [
     duree: "83",
   },
   {
+    image: "Discovery",
     nom: "Discovery",
     artiste: "Daft Punk",
     label: "Parlophone",
@@ -49,4 +55,30 @@ const tabAlbum = [
   },
 ];
 
-document.addEventListener("DOMContentLoaded", function () {});
+function display() {
+  let cd = document.querySelector("ul");
+  cd.innerHTML = "";
+  for (let album of albums) {
+    cd.insertAdjacentHTML(
+      "beforeend",
+      `<li>
+            <article>
+                <img src="img/${album.image}.jpg" alt="photo album">
+                <div class="infos">
+                    <p id="Nom">${album.nom}</p>
+                    <p id="Artiste">${album.artiste}</p>
+                    <p id="Label">${album.label}</p>
+                    <p id="AnnéeParution">${album.annee}</p>
+                    <p id="Genre">${album.genre}</p>
+                    <p id="DuréeTotale">${album.duree}</p>
+                </div>
+            </article>
+        </li>`
+    );
+  }
+  document.querySelectorAll("li").innerHTML = cd;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  display();
+});
